e06522dfd1f1ba117bd70bebca1ac6b86b1a6532
vnjotose <vicenlucunya@hotmail.com>
2014-02-06 20:57:28 -0500

    initial commit onemonthrails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinteresting::Application.config.secret_key_base = '11977afa79c18d6ad612594e13513e4d9c19f931d285f118f8de47111bda5693e521e7ee5c24d801434dbde9ca220fe7bd6848a92a59a00c1a6c06a1c705f0e9'
